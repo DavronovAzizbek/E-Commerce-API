174568a8530594c151e4ad6665244f9efd165213
githubusername <azizbekdavronov1110@gmail.com>
2025-04-21 22:14:18 +0500

    Database connected

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
